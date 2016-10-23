4e6caac5587d4f315471a0e9f0abdd6eff53a53a
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-23 17:23:30 +0200

    Setup unit test

---
+++ src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
@@ -0,0 +1,12 @@
+package com.mathieupauly.leapyearkata;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class LeapYearTest {
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+}

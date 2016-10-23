fabc9cc9e60de64ed1e10232fc2bd462db31dc51
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-23 18:33:46 +0200

    Refactor: prepare parameter extraction of expected values

--- src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
+++ src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
@@ -7,22 +7,22 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class LeapYearTest {
     @Test
     public void year_2001_should_not_be_leap() {
-        assertThat(isLeapYear(2001)).isFalse();
+        assertThat(isLeapYear(2001)).isEqualTo(false);
     }
 
     @Test
     public void year_1996_should_be_leap() {
-        assertThat(isLeapYear(1996)).isTrue();
+        assertThat(isLeapYear(1996)).isEqualTo(true);
     }
 
     @Test
     public void year_1900_should_be_leap() {
-        assertThat(isLeapYear(1900)).isFalse();
+        assertThat(isLeapYear(1900)).isEqualTo(false);
     }
 
     @Test
     public void year_2000_should_be_leap() {
-        assertThat(isLeapYear(2000)).isTrue();
+        assertThat(isLeapYear(2000)).isEqualTo(true);
     }
 
     private boolean isLeapYear(int year) {

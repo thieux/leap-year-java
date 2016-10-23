640e8fa6ef61a03e5bcada7bbad1c59a8027d73c
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-23 18:45:12 +0200

    Refactor: prepare for parameterized test (extract input & expected output as fields)

--- src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
+++ src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class LeapYearTest {
+
+    private int year;
+    private boolean expected;
+
+    public LeapYearTest() {
+        year = 2001;
+        expected = false;
+    }
+
     @Test
     public void year_2001_should_not_be_leap() {
-        assertThat(isLeapYear(2001)).isEqualTo(false);
+        assertThat(isLeapYear(year)).isEqualTo(expected);
     }
 
     @Test

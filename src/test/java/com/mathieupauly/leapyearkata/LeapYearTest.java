4bd2c0830132be4cb67f6f7b3291eb3e18ede138
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-23 18:17:04 +0200

    Refactor: simplify if

--- src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
+++ src/test/java/com/mathieupauly/leapyearkata/LeapYearTest.java
@@ -21,10 +21,7 @@ public class LeapYearTest {
     }
 
     private boolean isLeapYear(int year) {
-        if (year % 100 == 0) {
-            return false;
-        }
-        return year % 4 == 0;
+        return year % 100 != 0 && year % 4 == 0;
     }
 
 }
